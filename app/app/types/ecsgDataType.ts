6fce96fcbb58908bb05c9f5fcbfbdd290e69e5b8
sakuya for mac <>
2022-02-01 00:56:16 +0900

    :+1: プロジェクトの設定の型作成

---
+++ app/app/types/ecsgDataType.ts
@@ -0,0 +1,89 @@
+export type layerType = 'description' | 'colorPallete' | 'fontfamily' | 'typography' | string
+
+export type fontWeight = 'Thin' | 'Extra-light' | 'Light' | 'Regular' | 'Medium' | 'Semi-bold' | 'Bold' | 'Extra-bold' | 'Black'
+
+type commonType = {
+  isHide?: boolean
+  isLock?: boolean
+  order: string[]
+}
+
+// 
+// Project Settings
+// 
+
+export type projectType = {
+  name: string
+  order: layerType[]
+}
+
+// 
+// Color Pallete
+// 
+
+export type colorPalletType = {
+  colors: colorsType[]
+} & commonType
+
+type colorsType = {
+  id: string
+  title: string
+  comment: string
+  order: string[]
+  data: {
+    id: string
+    darkColor: string
+    lightColor: string
+    comment: string
+  }[]
+}
+
+// 
+// Font Family
+// 
+
+export type fontfamilyType = {
+  data: {
+    id: string
+    family: string
+    weight: fontWeight[]
+  }[]
+  css: string[]
+} & commonType
+
+// 
+// Typography
+// 
+
+export type typographyType = {
+  data: {
+    id: string
+    title: string
+    type: 'heading' | 'text' | 'code'
+    weight: fontWeight
+    size: number
+    unit:  'px' | 'pt' | 'rem' | 'em'
+  }[]
+} & commonType
+
+// 
+// Other
+// 
+
+export type otherType = {
+  title: string
+  data: {
+    id: string
+    img: string
+  }[]
+} & commonType
+
+export type projectDataType = {
+  project: projectType
+  data: {
+    colorPallet: colorPalletType
+    fontfamily: fontfamilyType
+    typography: typographyType
+    others: otherType[]
+  }
+}
