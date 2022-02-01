b75956fa7ad06b577f82f924e6afca56288556c8
sakuya for mac <>
2022-02-01 13:10:20 +0900

    :+1: Logoを作成

---
+++ app/app/components/parts/logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoMark from '~/assets/images/logo.svg'
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={LogoMark} alt="ECSG" />
+    </div>
+  );
+};
+
+export default Logo;
