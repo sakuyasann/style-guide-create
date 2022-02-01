ecda29e3f35de01058f7262cba997a4cca2bb5a9
sakuya for mac <>
2022-02-01 14:45:44 +0900

    :+1: Stack Componentを作成

---
+++ app/app/components/layout/stack/index.tsx
@@ -0,0 +1,30 @@
+import { css, jsx } from '@emotion/react';
+import React from 'react';
+
+type Props = {
+  spacing?: number
+  direction?: 'row' | 'column'
+}
+
+const index: React.FC<Props> = (props) => {
+  const {
+    children
+  } = props
+
+  const styles = {
+    base: css`
+      display: flex;
+      flex-direction: ${props.direction ? props.direction : 'column'};
+      gap: ${props.spacing ? `${props.spacing * .25}rem` : ''};
+    `
+  }
+
+  return (
+    <>
+      {/* @jsx jsx */}
+      <div css={styles.base}>{children}</div>
+    </>
+  );
+};
+
+export default index;
--- app/app/components/parts/index.ts
+++ app/app/components/parts/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './button'
 export { default as Logo } from './logo'
+export { default as Spacer } from './spacer'
