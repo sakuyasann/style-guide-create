ea41bb62c12b1dafa634cf534e83a13c0c2301ca
sakuya for mac <>
2022-02-01 15:33:25 +0900

    :+1: ToggleColorComponent 作成

---
+++ app/app/components/layout/header/togglecolor/index.tsx
@@ -0,0 +1,20 @@
+import { css, jsx } from '@emotion/react';
+import React from 'react';
+import { RiMoonFill, RiSunFill } from 'react-icons/ri';
+import { Button } from '~/components/parts';
+import { useColorMode } from '~/hooks';
+
+const ToggleColorButton = () => {
+  const {colorMode, toggleColorMode} = useColorMode()
+
+  return (
+    <>
+      {/* @jsx jsx */}
+      <Button onClick={toggleColorMode}>
+        {colorMode === 'dark' ? <RiSunFill /> : <RiMoonFill />}
+      </Button>
+    </>
+  );
+};
+
+export default ToggleColorButton;
