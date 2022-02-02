12b0141458e4b606ae5cc97983675eff4030bcf2
sakuya for mac <>
2022-02-02 19:03:37 +0900

    :+1: 必要なファイル作成

---
+++ app/app/components/layout/toolbar/description/index.tsx
@@ -0,0 +1,13 @@
+import { css, jsx } from '@emotion/react';
+import React from 'react';
+
+const DescriptionTool = () => {
+  return (
+    <>
+      {/* @jsx jsx */}
+
+    </>
+  );
+};
+
+export default DescriptionTool;
---
+++ app/app/components/layout/toolbar/typography/index.tsx
@@ -0,0 +1,13 @@
+import { css, jsx } from '@emotion/react';
+import React from 'react';
+
+const TypographyTool = () => {
+  return (
+    <>
+      {/* @jsx jsx */}
+
+    </>
+  );
+};
+
+export default TypographyTool;
